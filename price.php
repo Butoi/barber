34cb18836522f6ff5a9f726e1cfc404cdd32a496
Butoi <38652410+Butoi@users.noreply.github.com>
2018-04-27 01:24:31 +0100

    Create price.php

---
+++ price.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title>price</title>
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <link rel="stylesheet" type="text/css" href="styleee3.css">
+
+</head>
+<body>
+
+        <header>
+              <div id="header-inner">
+                     <nav>
+                            <ul>
+                                 <li>Address Street, Barber City GS23 </li>
+                                 <li>Call us on 386 40 556 558 </li>
+                                 <li>info@barber.com</li>
+
+                             </ul>
+                     </nav> 
+              </div>
+
+       
+              <div id="header-inner2">
+                     <nav >
+                            <ul>
+                                 <li id="header-inner3"><a href="index.php">Home</a></li>
+                                 <li id="header-inner3"><a href="gallery.php">Gallery</a></li>
+                                 <li id="header-inner3"><a href="price.php">Price</a></li>
+                                 <li id="header-inner3"><a href="appoinment">Book An Appoinment</a></li>
+                             </ul>
+                     </nav> 
+              </div>
+
+       </header>
+
+       <section id ="book">
+                 <h2>CHECK OUR SERVICES AND PRICES (All)</h2>
+
+
+       </section>
+
+       <section class="inner-wraper">
+                                    <article id="button">
+                                                           <button id="changeTexter1">CLASSIC HAIR STYLING - £49</button>
+                                                           <button id="changeTexter2">BEARD TRIMMING - £29</button>
+                                                           <button id="changeTexter3">MUSTACHE STYLING - £19</button>
+                                                           <button id="changeTexter4">COMPLETE TREATMENT - £49</button>
+                                                           <button id="changeTexter5">BEARD SHAVING - £19</button>
+                                                           <button id="changeTexter6">HAIR COLORING - £39</button>
+                                                           <button id="changeTexter7">JUST A FAST TRIM - £9</button>
+
+
+
+
+                                    </article>
+                                    <aside id="firstDiv">
+                                                     Haircut, shampoo/conditioner & hot towel finish. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam.<br>
+        <img src = "https://i2.wp.com/youmeandtrends.com/wp-content/uploads/2015/10/Beard-trimming-scissors-or-beard-trimmer.jpg" width="250px" height="210px">
+        <img src = "http://beard-style.com/image/1/2885-here-are-the-most-popular-beard-styles-over-the-past-16-decades-mic.jpg" width="250px" height="210px">
+        <img src = "https://www.living-postcards.com/sites/default/files/imagecache/art_inside/slide_homepage_1240x800_0.jpg" width="250px" height="210px">
+
+
+
+                                                <div id ="firstDiv"></div>
+
+        
+                                                <div id ="firstDiv"></div>
+
+        
+                                                <div id ="firstDiv"></div>
+
+        
+                                                <div id ="firstDiv"></div>
+
+        
+                                                <div id ="firstDiv"></div>
+
+       
+                                                <div id ="firstDiv"></div>
+
+
+        <script>
+                document.getElementById("changeTexter1").onclick=function(){
+                	document.getElementById("firstDiv").innerHTML="Haircut, shampoo/conditioner & hot towel finish. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Haircut, shampoo/conditioner & hot towel finish."+ "<br>" + "<img  src=\'https://i2.wp.com/youmeandtrends.com/wp-content/uploads/2015/10/Beard-trimming-scissors-or-beard-trimmer.jpg'\  width=\'250px\' height=\'210px\'>"  + "<img   src=\'http://beard-style.com/image/1/2885-here-are-the-most-popular-beard-styles-over-the-past-16-decades-mic.jpg '\  width=\'250px\' height=\'210px\'>" + "<img   src=\'https://www.living-postcards.com/sites/default/files/imagecache/art_inside/slide_homepage_1240x800_0.jpg'\  width=\'250px\' height=\'210px\'>" ; 
+                }
+
+                 document.getElementById("changeTexter2").onclick=function(){
+                	document.getElementById("firstDiv").innerHTML="Haircut, shampoo/conditioner & hot towel finish. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. ." + "<br>" + "<img  src=\'https://www.living-postcards.com/sites/default/files/imagecache/art_inside/slide_homepage_1240x800_0.jpg'\  width=\'250px\' height=\'210px\'>"  + "<img   src=\' http://www.cheatsheet.com/wp-content/uploads/2015/08/Bearded-man-in-a-barber-shop-640x426.jpg?x11342 '\  width=\'250px\' height=\'210px\'>" + "<img   src=\'https://atozhairstyles.com/wp-content/uploads/2016/08/4-Beard-Style-for-Oval-Face.jpg'\  width=\'250px\' height=\'210px\'>";
+                }
+
+                 document.getElementById("changeTexter3").onclick=function(){
+                	document.getElementById("firstDiv").innerHTML="Haircut, shampoo/conditioner & hot towel finish. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat." + "<br>" + "<img  src=\'https://i2.wp.com/youmeandtrends.com/wp-content/uploads/2015/10/Beard-trimming-scissors-or-beard-trimmer.jpg'\  width=\'250px\' height=\'210px\'>"  + "<img   src=\' http://beard-style.com/image/1/2885-here-are-the-most-popular-beard-styles-over-the-past-16-decades-mic.jpg '\  width=\'250px\' height=\'210px\'>" + "<img   src=\'https://www.living-postcards.com/sites/default/files/imagecache/art_inside/slide_homepage_1240x800_0.jpg'\  width=\'250px\' height=\'210px\'>";
+                }
+
+                 document.getElementById("changeTexter4").onclick=function(){
+                	document.getElementById("firstDiv").innerHTML="Haircut, shampoo/conditioner & hot towel finish. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat." + " <br>" + "<img  src=\'https://www.living-postcards.com/sites/default/files/imagecache/art_inside/slide_homepage_1240x800_0.jpg'\  width=\'250px\' height=\'210px\'>"  + "<img   src=\' http://www.cheatsheet.com/wp-content/uploads/2015/08/Bearded-man-in-a-barber-shop-640x426.jpg?x11342 '\  width=\'250px\' height=\'210px\'>" + "<img   src=\'https://atozhairstyles.com/wp-content/uploads/2016/08/4-Beard-Style-for-Oval-Face.jpg'\  width=\'250px\' height=\'210px\'>";
+                }
+
+                 document.getElementById("changeTexter5").onclick=function(){
+                	document.getElementById("firstDiv").innerHTML="Haircut, shampoo/conditioner & hot towel finish. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. " + "<br>" + "<img  src=\'https://i2.wp.com/youmeandtrends.com/wp-content/uploads/2015/10/Beard-trimming-scissors-or-beard-trimmer.jpg'\  width=\'250px\' height=\'210px\'>"  + "<img   src=\'  http://beard-style.com/image/1/2885-here-are-the-most-popular-beard-styles-over-the-past-16-decades-mic.jpg '\  width=\'250px\' height=\'210px\'>" + "<img   src=\'https://www.living-postcards.com/sites/default/files/imagecache/art_inside/slide_homepage_1240x800_0.jpg'\  width=\'250px\' height=\'210px\'>";
+                }
+
+                 document.getElementById("changeTexter6").onclick=function(){
+                	document.getElementById("firstDiv").innerHTML="Prices, products and offers may vary per location. Call your local Barber's Salon for prices. We offer various haircuts and beard trimming of all sorts. But our specialty is the proper trimming of your beard. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod. Haircut, shampoo/conditioner & hot towel finish." + "<br>" + "<img  src=\'https://www.living-postcards.com/sites/default/files/imagecache/art_inside/slide_homepage_1240x800_0.jpg'\  width=\'250px\' height=\'210px\'>"  + "<img   src=\' http://www.cheatsheet.com/wp-content/uploads/2015/08/Bearded-man-in-a-barber-shop-640x426.jpg?x11342 '\  width=\'250px\' height=\'210px\'>" + "<img   src=\'https://atozhairstyles.com/wp-content/uploads/2016/08/4-Beard-Style-for-Oval-Face.jpg'\  width=\'250px\' height=\'210px\'>" 
+                }
+
+                 document.getElementById("changeTexter7").onclick=function(){
+                	document.getElementById("firstDiv").innerHTML="Call your local Barber's Salon for prices. We offer various haircuts and beard trimming of all sorts. But our specialty is the proper trimming of your beard. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod. Haircut, shampoo/conditioner & hot towel finish ." + "<br>" + "<img  src=\'https://i2.wp.com/youmeandtrends.com/wp-content/uploads/2015/10/Beard-trimming-scissors-or-beard-trimmer.jpg'\  width=\'250px\' height=\'210px\'>"  + "<img   src=\'  http://beard-style.com/image/1/2885-here-are-the-most-popular-beard-styles-over-the-past-16-decades-mic.jpg '\  width=\'250px\' height=\'210px\'>" + "<img   src=\'https://www.living-postcards.com/sites/default/files/imagecache/art_inside/slide_homepage_1240x800_0.jpg'\  width=\'250px\' height=\'210px\'>";
+                	document.getElementById("s").innerHTML="1px";
+                
+                }
+
+            
+
+         </script>
+
+
+
+
+         </aside>
+
+
+       </section>
+
+
+
+          <section class="inner-wraper2">
+
+              <section class="one-third">
+                        <h3>OUR INSTAGRAM FEED</h3>
+                         <img src="https://images.pexels.com/photos/46710/pexels-photo-46710.jpeg?h=350&auto=compress&cs=tinysrgb" width="70" height="70">
+                         <img src="https://www.nationalgeographic.com/content/dam/travel/2016-digital/canada/alberta/alberta-road-trip/icefields-parkway-lake-louise-alberta-canada.adapt.1190.1.jpg" width="70" height="70">
+                         <img src="https://thumbs.dreamstime.com/z/hipster-girl-laptop-cup-coffee-ona-wooden-table-close-up-60691062.jpg" width="70" height="70"><br>
+                         <img src="https://images.pexels.com/photos/296879/pexels-photo-296879.jpeg?w=940&h=650&auto=compress&cs=tinysrgb" width="70" height="70">
+                         <img src="https://cdn-images-1.medium.com/max/2000/1*ZBXLF0FmRoZWdHW8yfLbeA.jpeg" width="70" height="70">
+                         <img src="https://funalive.com/uploads/files/article/images/funny-interesting-pictures_24sep16-4.jpg" width="70" height="70"><br>
+                         <img src =  "https://brashiermiddlecollegenews.org/wp-content/uploads/2017/01/nice.png" width="70" height="70">
+                         <img src =  "https://cdn.tinybuddha.com/wp-content/uploads/2015/06/Happy-Woman.jpg" width="70" height="70">
+                         <img src =  "https://thehappypharmacist.files.wordpress.com/2016/03/1024x768-data-uploads-35-401510-im-happy-best-wallpaper.jpg" width="70" height="70">
+            
+              </section>
+
+                <section class="one-third">
+                        <h3>ABOUT US</h3>
+                         
+                                             <p id = "totedd">Barber Saloon is a vintage design<br> barber that focuses on your beard's<br> best experience.<br>
+                                                 (+386) 40 123 456<p>
+                                                  
+
+                                                <p id = "totedd"><span class="fa fa-envelope" style="font-size:20px"></span>info@barber.com</p>
+                                                <p id = "totedd"> <span class="fa fa-phone" style="font-size:20px"></span>www.barber.com</p>
+                </section>
+
+                <section class="one-third">
+                        <h3>OPENING TIMES</h3>
+                                                  <p id = "totedd">Monday <span id = "asas"> 10:00 - 16:00</span></p>
+                                                  <p id = "totedd">Tuesday <span id = "asas"> 10:00 - 16:00</span></p> 
+                                                  <p id = "totedd">Wednesday  <span id = "asas"> 08:00 - 15:00</span></p>
+                                                  <p id = "totedd">Thursday <span id = "asas">  08:00 - 18:00</span></p>
+                                                  <p id = "totedd">Friday <span id = "asas">  07:00 - 19:00</span></p>
+                                                  <p id = "totedd">Saturday  <span id = "asas"> CLOSED</span></p>
+                                                  <p id = "totedd">Sunday    <span id = "asas">CLOSED</span></p>
+                       
+                </section>
+
+
+            </section>
+
+            <footer>
+                     <h5 id = "bottome3">TRIM YOUR BEARD AT  THE BARBER SALOON</h5>
+                     <h5 id = "bottome4">TRIM YOUR BEARD AT  THE BARBER SALOON</h5>
+                     <a href="appoinment.php"><button onclick="topFunction()" id="show">Book now.</button></a>
+            </footer>
+
+            <!--End  footer section-->
+
+
+</body>
+</html>
