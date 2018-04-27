394700bd1f5480ab50ce83bbea45edd94f807d5b
Butoi <38652410+Butoi@users.noreply.github.com>
2018-04-27 01:23:53 +0100

    Create gallery.php

---
+++ gallery.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+<head>
+       <title>Gallery</title>
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <link rel="stylesheet" type="text/css" href="styleee2.css">
+  </head>
+<body>
+
+          <header>
+              <div id="header-inner">
+                     <nav>
+                            <ul>
+                                 <li>Address Street, Barber City GS23 </li>
+                                 <li>Call us on 386 40 556 558 </li>
+                                 <li>info@barber.com</li>
+
+                             </ul>
+                     </nav> 
+              </div>
+
+       
+              <div id="header-inner2">
+                     <nav >
+                            <ul>
+                                 <li id="header-inner3"><a href="index.php">Home</a></li>
+                                 <li id="header-inner3"><a href="gallery.php">Gallery</a></li>
+                                 <li id="header-inner3"><a href="price.php">Price</a></li>
+                                 <li id="header-inner3"><a href="appoinment.php">Book An Appoinment</a></li>
+                             </ul>
+                     </nav> 
+              </div>
+
+       </header>
+       <!--End of headers-->
+
+       <section class="inner-wraper">
+
+                       
+                             <img id ="gallery1" src = "http://shopshiptoshore.com/wp-content/uploads/2016/06/iStock_000084252979_Medium.jpg" width="300px" height="300px" alt="Fjords">
+        
+     
+
+           
+                             <img id ="gallery1" src = "https://blog.thebeardclub.com/wp-content/uploads/2016/07/trimming_thumb2.jpg" width="300px" height="300px"  alt="Fjords">
+          
+          
+
+         
+                            <img id ="gallery1" src = "http://legendsmenshaircuts.com/assets/beard-trimming-keego-harbor-mi.jpg" width="300px" height="300px"  alt="Fjords">
+         
+       
+
+                            <img id ="gallery1" src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTTzjtKy98IBBxcz1NmvcL6ZWa3LD-6nhIPbR9hoV6RFpZblhL4Q"  width="300px" height="300px"  alt="Fjords" >
+        
+        
+
+                            <img id = "gallery1" src = "http://www.mistershaver.com/wp-content/uploads/beard-trimming.jpg" width="300px" height="300px" alt="Fjords" >
+            
+          
+                            <img id ="gallery1" src = "https://img.huffingtonpost.com/asset/58a719a42900002800f27126.jpg?ops=scalefit_720_noupscale" width="300px" height="300px" alt="Fjords">
+          
+
+           
+                            <img id ="gallery1" src = "https://fthmb.tqn.com/9hk4xanB2-YOmTtKYpQkHaEbgrY=/3867x2581/filters:no_upscale():fill(FFCC00,1)/barber-cutting-hair-56a6110a3df78cf7728b2d97.jpg" width="300px" height="300px" alt="Fjords">
+        
+    
+                            <img id ="gallery1" src = "http://hairsuite.net/wp-content/uploads/2016/03/Hair-Salon-1.jpg" width="300px" height="300px" alt="Fjords"> 
+         
+
+           
+                            <img  id ="gallery1" src = "http://shopshiptoshore.com/wp-content/uploads/2016/06/iStock_000084252979_Medium.jpg" width="300px" height="300px"  alt="Fjords">
+         
+      
+      
+                            <img  id ="gallery1" src = "https://thenypost.files.wordpress.com/2017/06/051717_brooklyn_tourettes_barber_pm-1.jpg?quality=90&strip=all&strip=all" width="300px" height="300px" alt="Fjords" >
+
+                            <img  id ="gallery1" src = "https://static01.nyt.com/images/2011/02/10/fashion/Z-SCOOP-E/Z-SCOOP-E-jumbo.jpg" width="300px" height="300px" alt="Fjords" >
+
+                            <img  id ="gallery1" src = "https://www.howtobecomeahairdresser.com.au/wp-content/uploads/2016/01/Hairdresser-Hairstylist-Barber-Whats-the-difference.jpg" width="300px" height="300px" alt="Fjords" >
+
+           
+
+
+
+       </section>
+
+         <section class="inner-wraper2">
+
+              <section class="one-third">
+                        <h3>OUR INSTAGRAM FEED</h3>
+                         <img id="responsive" src="https://images.pexels.com/photos/46710/pexels-photo-46710.jpeg?h=350&auto=compress&cs=tinysrgb" width="70" height="70">
+                         <img  id="responsive" src="https://www.nationalgeographic.com/content/dam/travel/2016-digital/canada/alberta/alberta-road-trip/icefields-parkway-lake-louise-alberta-canada.adapt.1190.1.jpg" width="70" height="70">
+                         <img  id="responsive" src="https://thumbs.dreamstime.com/z/hipster-girl-laptop-cup-coffee-ona-wooden-table-close-up-60691062.jpg" width="70" height="70"><br>
+                         <img   id="responsive" src="https://images.pexels.com/photos/296879/pexels-photo-296879.jpeg?w=940&h=650&auto=compress&cs=tinysrgb" width="70" height="70">
+                         <img   id="responsive" src="https://cdn-images-1.medium.com/max/2000/1*ZBXLF0FmRoZWdHW8yfLbeA.jpeg" width="70" height="70">
+                         <img  id="responsive"  src="https://funalive.com/uploads/files/article/images/funny-interesting-pictures_24sep16-4.jpg" width="70" height="70"><br>
+                         <img  id="responsive" src =  "https://brashiermiddlecollegenews.org/wp-content/uploads/2017/01/nice.png" width="70" height="70">
+                         <img   id="responsive" src =  "https://cdn.tinybuddha.com/wp-content/uploads/2015/06/Happy-Woman.jpg" width="70" height="70">
+                         <img  id="responsive"  src =  "https://thehappypharmacist.files.wordpress.com/2016/03/1024x768-data-uploads-35-401510-im-happy-best-wallpaper.jpg" width="70" height="70">
+          	
+              </section>
+
+                <section class="one-third">
+                        <h3>ABOUT US</h3>
+                         
+                                             <p id = "totedd">Barber Saloon is a vintage design<br> barber that focuses on your beard's<br> best experience.<br>
+                                                 (+386) 40 123 456<p>
+                                                  
+
+                                                <p id = "totedd"><span class="fa fa-envelope" style="font-size:20px"></span>info@barber.com</p>
+                                                <p id = "totedd"> <span class="fa fa-phone" style="font-size:20px"></span>www.barber.com</p>
+                </section>
+
+                <section class="one-third">
+                        <h3>OPENING TIMES</h3>
+                                                  <p id = "totedd">Monday <span id = "asas"> 10:00 - 16:00</span></p>
+                                                  <p id = "totedd">Tuesday <span id = "asas"> 10:00 - 16:00</span></p> 
+                                                  <p id = "totedd">Wednesday  <span id = "asas"> 08:00 - 15:00</span></p>
+                                                  <p id = "totedd">Thursday <span id = "asas">  08:00 - 18:00</span></p>
+                                                  <p id = "totedd">Friday <span id = "asas">  07:00 - 19:00</span></p>
+                                                  <p id = "totedd">Saturday  <span id = "asas"> CLOSED</span></p>
+                                                  <p id = "totedd">Sunday    <span id = "asas">CLOSED</span></p>
+                       
+                </section>
+
+
+            </section>
+
+            <footer>
+                     <h5 id = "bottome3">TRIM YOUR BEARD AT  THE BARBER SALOON</h5>
+                     <h5 id = "bottome4">TRIM YOUR BEARD AT  THE BARBER SALOON</h5>
+                     <a href="appoinment.php"<button onclick="topFunction()" id="show">Book now.</button></a> 
+            </footer>
+
+            <!--End  footer section-->
+
+
+</body>
+</html>
